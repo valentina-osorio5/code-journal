a7956cf26fb1689703ac3ab2396d8719bb757a1c
valentina osorio <va.os.consulting@gmail.com>
2024-11-12 15:08:47 +0000

    in progress responsive design and finished event listener issue #1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 "use strict";
 const $image = document?.querySelector('#entry-image');
-console.log($image);
 const $imageUrl = document?.querySelector('#photo-url');
+const $entryForm = document?.querySelector('#entry-form');
 function handleInput(event) {
     const eventTarget = event.target;
     const newSrc = eventTarget.value;
-    console.log(newSrc);
-    $imageUrl?.setAttribute('src', 'eventTarget.value');
+    $image?.setAttribute('src', newSrc);
 }
 $imageUrl?.addEventListener('input', handleInput);
+const $button = document?.querySelector('.button');
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log('button was pressed');
+    const $formElements = $entryForm?.elements;
+    const obj = {
+        title: $formElements.title.value,
+        photoUrl: $formElements.photoUrl.value,
+        notes: $formElements.notes.value,
+    };
+    console.log(obj);
+}
+$entryForm?.addEventListener('submit', handleSubmit);
--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,31 @@
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  photoUrl: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
+
 const $image = document?.querySelector('#entry-image');
-console.log($image);
 const $imageUrl = document?.querySelector('#photo-url');
+const $entryForm = document?.querySelector('#entry-form') as HTMLFormElement;
 
 function handleInput(event: any): void {
   const eventTarget = event.target as HTMLInputElement;
   const newSrc = eventTarget.value;
-  console.log(newSrc);
-  $imageUrl?.setAttribute('src', 'eventTarget.value');
+  $image?.setAttribute('src', newSrc);
 }
 $imageUrl?.addEventListener('input', handleInput);
+
+const $button = document?.querySelector('.button');
+
+function handleSubmit(event: any): void {
+  event.preventDefault();
+  console.log('button was pressed');
+  const $formElements = $entryForm?.elements as FormElements;
+  const obj = {
+    title: $formElements.title.value,
+    photoUrl: $formElements.photoUrl.value,
+    notes: $formElements.notes.value,
+  };
+  console.log(obj);
+}
+$entryForm?.addEventListener('submit', handleSubmit);
