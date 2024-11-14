30a9c9b692cfc274a83af27b3ba898aff2b77205
valentina osorio <va.os.consulting@gmail.com>
2024-11-14 01:35:32 +0000

    completed issue/feature 2

--- ts/main.ts
+++ ts/main.ts
@@ -19,6 +19,9 @@ const $uList = document?.querySelector('ul');
 const $dataViewDiv = document?.querySelector('.dataviewentries');
 const $holdsNoEntries = document?.querySelector('.holds-no-entries');
 const $navBarButton = document?.querySelector('.navbarbtn');
+const $newEntryButton = document?.querySelector('.newentrybtn');
+const $newEntries = document?.querySelector('.entries-styling');
+const $liItem = document?.querySelectorAll('li');
 
 function handleInput(event: any): void {
   const eventTarget = event.target as HTMLInputElement;
@@ -42,7 +45,6 @@ function handleSubmit(event: any): void {
   $uList?.prepend(renderEntry(newEntry));
   data.nextEntryId++;
   writeData();
-  // handleDCL();
   $image?.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
   viewSwap('entries');
@@ -70,71 +72,56 @@ function renderEntry(entry: Entry): any {
   return listItem;
 }
 
-// console.log(renderEntry(testEntries));
-
-// const testEntries = {
-//   entryId: 1,
-//   title: 'test',
-//   photoUrl:
-//     'https://plus.unsplash.com/premium_photo-1664382466516-756b1e0721f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8',
-//   notes: 'test',
-// };
-
 window.addEventListener('DOMContentLoaded', handleDCL);
 
 function handleDCL(): void {
-  // console.log('handleDCLfired');
+  toggleNoEntries();
   for (let i = 0; i < data.entries.length; i++) {
-    // console.log('looping through entries');
     const listItem = renderEntry(data.entries[i]);
     $uList?.appendChild(listItem);
-    // console.log('I added to the UL element');
+    viewSwap(currentView);
   }
+}
+const currentView = data.view;
+
+function viewSwap(viewName: 'entries' | 'entry-form'): void {
+  console.log('view swap fired');
+  console.log(viewName);
+  const entriesView = document.getElementById('entries');
+  const entryFormView = document.getElementById('entry-form-view');
+  //   // Hide or show the appropriate view based on viewName
+  if (viewName === 'entries') {
+    entriesView?.classList.remove('hidden');
+    $newEntries?.classList.remove('hidden');
+    entryFormView?.classList.add('hidden');
+  } else if (viewName === 'entry-form') {
+    entryFormView?.classList.remove('hidden');
+    entriesView?.classList.add('hidden');
+    $newEntries?.classList.add('hidden');
+  }
+
+  // Update the view in the data model
+  data.view = viewName;
+  toggleNoEntries();
 }
 
 function toggleNoEntries(): void {
   if (data.entries.length === 0) {
     console.log('showing no entries');
-    if ($holdsNoEntries) {
-      $holdsNoEntries.className = 'column-full holds-no-entries';
-    }
-    if ($dataViewDiv) {
-      $dataViewDiv.className = 'dataviewentries hidden';
-    }
+    $holdsNoEntries?.classList.remove('hidden');
   } else {
     console.log('hiding no entries div, showing entries');
-    if ($dataViewDiv) {
-      $dataViewDiv.className = 'dataviewentries';
-    }
-    if ($holdsNoEntries) {
-      $holdsNoEntries.className = 'column-full holds-no-entries hidden';
-    }
+    $holdsNoEntries?.classList.add('hidden');
   }
 }
-
-// toggleNoEntries();
-
-// function viewSwap(viewName: 'entries' | 'entry-form'): void {
-//   console.log('view swap fired');
-//   console.log(viewName);
-//   const entriesView = document.getElementById('entries');
-//   const entryFormView = document.getElementById('entry-form-view');
-//   //   // Hide or show the appropriate view based on viewName
-//   if (viewName === 'entries') {
-//     entriesView?.classList.remove('hidden');
-//     entryFormView?.classList.add('hidden');
-//   } else if (viewName === 'entry-form') {
-//     entryFormView?.classList.remove('hidden');
-//     entriesView?.classList.add('hidden');
-//   }
-
-//     // Update the view in the data model
-//     data.view = viewName;
-//     toggleNoEntries();
-// }
 
 function handleViewEntriesClick(): void {
   viewSwap('entries');
 }
 
 $navBarButton?.addEventListener('click', handleViewEntriesClick);
+
+function handleNewEntry(): void {
+  viewSwap('entry-form');
+}
+$newEntryButton?.addEventListener('click', handleNewEntry);
